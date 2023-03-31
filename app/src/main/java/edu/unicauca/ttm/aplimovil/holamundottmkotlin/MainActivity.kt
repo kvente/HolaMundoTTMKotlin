88743c1b0d72498b9d1dcad1d9d377ee0191831f
Kevin Herney Vente Angulo <kevinthebestfriends@gmail.com>
2023-03-30 19:16:59 -0500

    primer commit de mi proyecto

---
+++ app/src/main/java/edu/unicauca/ttm/aplimovil/holamundottmkotlin/MainActivity.kt
@@ -0,0 +1,43 @@
+package edu.unicauca.ttm.aplimovil.holamundottmkotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.unicauca.ttm.aplimovil.holamundottmkotlin.ui.theme.HolaMundoTTMKotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HolaMundoTTMKotlinTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HolaMundoTTMKotlinTheme {
+        Greeting("Android")
+    }
+}
